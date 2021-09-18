871dc699c74408f4b7af257752fe9c0f8351a276
wanjema <jonwanjema@gmail.com>
2021-09-19 00:02:09 +0300

    add encode function

---
+++ encode.py
@@ -0,0 +1,13 @@
+
+# Python3 code to print
+# all encodings available
+ 
+from encodings.aliases import aliases
+ 
+# Printing list available
+print("The available encodings are : ")
+# print(aliases.keys())
+
+#encode
+print ("The encoded string in euc_jis2004 format is : ",)
+print ('Python is fun'.encode('ascii', 'ignore'))
