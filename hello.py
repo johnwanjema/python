b67b5e8622d9266d00ddfe2c99739d900d2ba982
wanjema <jonwanjema@gmail.com>
2021-09-18 23:33:18 +0300

    add python main function

---
+++ hello.py
@@ -0,0 +1,30 @@
+print("Hello World")
+
+# List is the most basic Data Structure in python.
+# List is a mutable data structure i.e items can be added to list later after the list creation.
+
+# creates a empty list
+nums = [] 
+  
+# appending data in list
+nums.append(21)
+nums.append(40.5)
+nums.append("String")
+  
+# print(nums)
+
+
+# Python program to illustrate
+# functions
+def hello():
+    name = input("Enter your name: ")
+    print("Hello", name)
+
+
+
+def Main():
+    hello()
+    
+
+if __name__=="__main__":
+    Main()
