8fbf18665872d62ee6e6d9fa2b8200a54ba0b541
wanjema <jonwanjema@gmail.com>
2021-09-21 14:22:14 +0300

    add method to format string

---
+++ stringformat.py
@@ -0,0 +1,15 @@
+
+# Python3 program to demonstrate
+# the str.format() method
+ 
+# using format option in a simple string
+print("{}, wanjema."
+      .format("John"))
+ 
+# using format option for a
+# value stored in a variable
+str = "This article is written in {}"
+print(str.format("Python"))
+ 
+# formatting a string using a numeric constant
+print("Hello, I am {} years old !".format(22))
